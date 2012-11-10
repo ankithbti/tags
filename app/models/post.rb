efc6fe6dab9e35fe031cc2719fd78e7b9021b4c5
ankithbti <ankithbti007@gmail.com>
2012-11-10 23:02:05 +0530

    Updated Tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :tag_list
+  
+  acts_as_taggable
+
   validates :title , presence: true
   validates :content , presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tags::Application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-
+  get 'tags/:tag', to: 'posts#index', as: :tag
   resources :posts
   root :to => 'posts#index'
 
